85730a7017f7a60b69077d02668bb4f524799a15
Lucas Janoca <Lucasjanoca9@gmail.com>
2026-08-30 20:44:21 -0300

    security: pin Supabase Edge runtime types in padoka-push

--- supabase/functions/padoka-push/index.ts
+++ supabase/functions/padoka-push/index.ts
@@ -1,4 +1,4 @@
-import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import "jsr:@supabase/functions-js@2.112.4/edge-runtime.d.ts";
 import { createClient } from "npm:@supabase/supabase-js@2.112.4";
 import { sendPushNotification, WebPushError } from "npm:@mmmike/web-push@1.3.0/send";
 
